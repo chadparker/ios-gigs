288fdcb6b5a889c5eafb1d26e62778d9fe7d6024
Chad Parker <chad@chadparker.com>
2020-03-17 19:40:04 -0700

    Add User and Bearer model files

---
+++ Gigs/Model/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Chad Parker on 3/17/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
---
+++ Gigs/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Chad Parker on 3/17/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
